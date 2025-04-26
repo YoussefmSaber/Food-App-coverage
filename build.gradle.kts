cf26f70ea26c27bcd026f76b35599733b97eaa5f
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-26 03:48:03 +0300

    add JaCoCo test coverage reporting and verification to CI pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,6 @@ dependencies {
     // kotlin date time
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
     // kotest, assertion
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
@@ -30,6 +28,7 @@ dependencies {
 
     // mockk
     testImplementation("io.mockk:mockk:1.14.0")
+
     // junit params
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
@@ -38,6 +37,49 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(20)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/generated/**")
+        }
+    )
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+    executionData.setFrom(fileTree(buildDir).include("jacoco/test.exec"))
+}
+
+tasks.register<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+    dependsOn(tasks.test)
+
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.80".toBigDecimal() // Require 80% line coverage
+            }
+        }
+    }
+
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/generated/**")
+        }
+    )
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+    executionData.setFrom(fileTree(buildDir).include("jacoco/test.exec"))
+}
+
+tasks.check {
+    dependsOn("jacocoTestReport", "jacocoTestCoverageVerification")
 }
